e31c940c801de9fd49950515af3c05bf5969429d
Gourav Sharma <23bcs10857@cuchd.in>
2025-11-14 19:17:26 +0530

    Delete Pet_Adoption_Platform_(Final Project) directory

--- Pet_Adoption_Platform_(Final Project)/backend/src/main/java/com/adoptionplatform/backend/repository/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.adoptionplatform.backend.repository;
-
-import com.adoptionplatform.backend.model.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetRepository extends JpaRepository<Pet, Long> {}
-
-
